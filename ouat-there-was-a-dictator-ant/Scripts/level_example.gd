0472910b7021749706426a35b902a7ae06caee21
AgonGuri <guriagon@gmail.com>
2025-12-19 22:53:22 +0100

    changing scenes works somehow

--- ouat-there-was-a-dictator-ant/Scripts/level_example.gd
+++ ouat-there-was-a-dictator-ant/Scripts/level_example.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-var north_room = preload("res://Scenes/level_example.tscn")
-var south_room = preload("res://Scenes/level_example.tscn")
-var east_room = preload("res://Scenes/level_example.tscn")
-var west_room = preload("res://Scenes/level_example.tscn")
+var north_room = "res://Scenes/level_example.tscn"
+var south_room = "res://Scenes/level_example.tscn"
+var east_room = "res://Scenes/level_example.tscn"
+var west_room = "res://Scenes/level_example.tscn"
 
 
 func _on_north_area_entered(area: Area2D) -> void:
-	pass
+	print("entered")
+	TransitionScene.change_scene(north_room)
 
 
 func _on_south_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	TransitionScene.change_scene(south_room)
 
 
 func _on_east_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	TransitionScene.change_scene(east_room)
 
 
 func _on_west_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	TransitionScene.change_scene(west_room)
