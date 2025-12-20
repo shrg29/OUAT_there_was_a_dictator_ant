1bb6d5e532c36ec2ff4a806e36bfe8a80dd84d45
AgonGuri <guriagon@gmail.com>
2025-12-20 18:23:10 +0100

    loading is somewhat broken
    sometimes it works, sometimes it doesnt

---
+++ ouat-there-was-a-dictator-ant/Scripts/room.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+func _on_north_area_entered(area: Area2D) -> void:
+	print("entered")
+	World.load_room("N")
+
+
+func _on_south_area_entered(area: Area2D) -> void:
+	World.load_room("S")
+
+
+func _on_east_area_entered(area: Area2D) -> void:
+	World.load_room("E")
+
+
+func _on_west_area_entered(area: Area2D) -> void:
+	World.load_room("W")
