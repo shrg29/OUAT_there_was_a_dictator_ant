7b5f9f872712e61de8aad24f840fde8e1a65283f
Gandaris <pasa.mieslinger@gmail.com>
2025-12-21 14:07:02 +0100

    item display

--- ouat-there-was-a-dictator-ant/Scripts/recruited_ants.gd
+++ ouat-there-was-a-dictator-ant/Scripts/recruited_ants.gd
@@ -8,7 +8,7 @@ var screen_index: int = 1
 @onready var screen_four = $HBoxContainer/Control4/SubViewportContainer/screen_four
 @onready var screen_five = $HBoxContainer/Control5/SubViewportContainer/screen_five
 
-@onready var anthony = preload("res://Scenes/npc/npc_1.tscn")
+@onready var anthony = preload("res://Scenes/npc/npc_3.tscn")
 @onready var antdrew = preload("res://Scenes/npc/npc_2.tscn")
 @onready var antonia = preload("res://Scenes/npc/npc_3.tscn")
 @onready var anta = preload("res://Scenes/npc/npc_4.tscn")
@@ -23,17 +23,18 @@ var screen_index: int = 1
 
 
 func _ready() -> void:
+	await get_tree().process_frame
+	GameState.item_update.connect(update_item_holder)
 	pass
 
 
 func update_ant_display(name: String):
 	await get_tree().process_frame
 	var new_screen
-	screen_index = GameState.recruited_ants.size() - 1
+	screen_index = GameState.recruited_ants.size() + 1
 	match screen_index:
 		1:
 			new_screen = screen_one
-			print("reached first screen")
 		2:
 			new_screen = screen_two
 		3:
@@ -46,13 +47,9 @@ func update_ant_display(name: String):
 	
 	match name:
 		"Anthony":
-			print("loaded 0")
 			var scene = anthony.instantiate()
-			print("loaded 1")
-			#scene.position = Vector2(150,150)
-			print("loaded 2")
+			scene.position = Vector2(150,150)
 			new_screen.add_child(scene)
-			print("loaded 3")
 		"Antdrew":
 			var scene = antdrew.instantiate()
 			scene.position = Vector2(150,150)
@@ -74,3 +71,25 @@ func update_ant_display(name: String):
 			scene.position = Vector2(150,150)
 			new_screen.add_child(scene)
 			pass
+
+func update_item_holder(valid: bool):
+	if GameState.held_items.size() >= 6:
+		item_five.texture = GameState.held_items[5].item_texture
+	else:
+		item_five.texture = null
+	if GameState.held_items.size() >= 5:
+		item_four.texture = GameState.held_items[4].item_texture
+	else:
+		item_four.texture = null
+	if GameState.held_items.size() >= 4:
+		item_three.texture = GameState.held_items[3].item_texture
+	else:
+		item_three.texture = null
+	if GameState.held_items.size() >= 3:
+		item_two.texture = GameState.held_items[2].item_texture
+	else:
+		item_two.texture = null
+	if GameState.held_items.size() >= 2:
+		item_one.texture = GameState.held_items[1].item_texture
+	else:
+		item_one.texture = null
