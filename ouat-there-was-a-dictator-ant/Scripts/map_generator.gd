4ad091b0306a8cc36e3cf5df4e13591bcaa9afd3
Sara Hrg <hrgsara6@gmail.com>
2025-12-21 15:15:26 +0100

    timer fixed

--- ouat-there-was-a-dictator-ant/Scripts/map_generator.gd
+++ ouat-there-was-a-dictator-ant/Scripts/map_generator.gd
@@ -4,10 +4,6 @@ extends Node2D
 
 @export var map_transparency = 0.5
 @onready var mini_map = $minimap
-@onready var game_timer = $GameTimer
-@onready var timer_label = $TimerLabel
-
-var time_left: int
 
 func generate_mini_map():
 	AntHillGenerator.map_transparency = self.map_transparency
@@ -17,16 +13,3 @@ func generate_mini_map():
 
 func _ready() -> void:
 	generate_mini_map()
-	if timer_label:
-		timer_label.text = "Time: %d" % time_left
-	game_timer.start()
-
-func _process(delta):
-	# optional: update label every frame if you want live countdown
-	if timer_label:
-		var remaining = int(game_timer.time_left)
-		timer_label.text = "Time: %d" % remaining
-
-
-func _on_game_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
