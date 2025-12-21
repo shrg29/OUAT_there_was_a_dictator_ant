c8a56076fef2012aac52ca122a3e3c6dccc2b551
Sara Hrg <hrgsara6@gmail.com>
2025-12-21 13:18:47 +0100

    main menu connected to tutorial room

--- ouat-there-was-a-dictator-ant/Scripts/main_menu.gd
+++ ouat-there-was-a-dictator-ant/Scripts/main_menu.gd
@@ -7,7 +7,7 @@ func _ready():
 
 #go to main scene
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/tutorial_room.tscn")
 
 #quit
 func _on_quit_pressed():
